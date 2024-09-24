9488ab1341bba2e15b5f68a244310e9d0d4f507a
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-25 00:00:30 +0530

    Hello World

---
+++ 0.HelloWorld.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    // This is a single-line comment
+    printf("Hello, World!\n");  // This prints a message
+
+    /* 
+    This is a multi-line comment
+    It can span several lines
+    */
+    printf("Bye World!\n");
+    return 0;
+}
+
+
+
+/* 
+    \n	Newline (line break)
+    \t	Horizontal tab
+    \\	Backslash (\)
+    \"	Double quote (")
+    \'	Single quote (')
+    \r	Carriage return
+    \b	Backspace
+    \f	Form feed
+    \0	Null character
+*/
+
